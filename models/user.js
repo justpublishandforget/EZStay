f42e3ac8dee92ddb1057acb2490be4e7fb683556
VedNik3 <vedantnikam812@gmail.com>
2024-04-02 02:54:25 +0530

    login and signup and controller

--- models/user.js
+++ models/user.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    fullname:{
+        type : String,
+        require : true
+       },
     username:{
         type : String,
         require : true,
         unique : true
     },
+    email:{
+        type : String,
+        
+        require : true
+       },
+    phone:{
+        type : String,
+        require : true
+       },
+    dob:{
+        type : Date,
+        require : true
+       },
     password:{
         type : String,
         require : true 
--- routes/user.js
+++ routes/user.js
@@ -1,86 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const user = require('../controllers/user');
+
 
 
 // signup handler
-
-router.get('/signup', (req, res) => {
-  res.sendFile('signup.html', { root: global.staticpath });
-});
-
-router.post('/signup', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-      // Check if the username already exists
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-          return res.status(400).send('Username already exists. Please choose a different username.');
-      }
-
-      // Create a new user
-      const user = new User({ username, password });
-      await user.save();
-      res.redirect('/login');
-  } catch (err) {
-      console.error(err);
-      res.status(500).send(`Error signing up: ${err.message}`);
-  }
-});
+router.get('/signup', user.newUsers);
+router.post('/signup', user.createUser);
 
 
   // login handler
-
-router.get('/login', (req, res) => {
-    res.sendFile('login.html', { root: global.staticpath });
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username, password });
-    if (!user) {
-      res.status(401).send('Invalid username or password');
-    } else {
-      req.session.user = user;
-      
-      res.redirect('/index.html?username=' + encodeURIComponent(username));
-     
-    }
-  } catch (err) {
-    console.error(err);
-    
-    res.status(500).send(`Error loging in: ${err.message}`);
-  }
-});
+  router.get('/login', user.getUsers);
+  router.post('/login', user.accessUser);
 
 
 //index
-router.get('/index.html', (req, res) => {
-  if (!req.session.user) {
-    res.redirect('/login');
-  } else {
-    res.render('index.html', { username: req.session.user.username });
-  }
-});
-
-
+router.get('/index.html', user.index);
 
 
 // Logout
-router.get('/logout', (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error logging out');
-    } else {
-      res.redirect('/index.html');
-    }
-  });
-});
-
-
-
+router.get('/logout', user.logout);
 
 module.exports = router;
